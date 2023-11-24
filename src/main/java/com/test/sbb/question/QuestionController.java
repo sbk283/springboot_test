3739fa016c079495bd67785ed9ed0bb3973c06c0
sbk283 <sbk283@naver.com>
2023-11-24 12:23:47 +0900

    1차과제테스트완료

---
+++ src/main/java/com/test/sbb/question/QuestionController.java
@@ -0,0 +1,45 @@
+package com.test.sbb.question;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/question")
+public class QuestionController {
+
+    private final QuestionService questionService;
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList",questionList);
+        return "question_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
+    @GetMapping("/create")
+    public String qusetionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String qusetionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
+    }
+}
