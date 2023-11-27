a2b84d619ee6358c1f8a33337f43926c9ce2b6dd
sbk283 <133177283+sbk283@users.noreply.github.com>
2023-11-27 19:48:55 +0900

    테스트에 필요했던 부분 삭제

--- src/main/java/com/test/sbb/question/QuestionRepository.java
+++ src/main/java/com/test/sbb/question/QuestionRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    Question findBySubject(String subject);
-    Question findBySubjectAndContent(String subject, String content);
-    List<Question> findBySubjectLike(String subject);
+    
 }
